9cd196eefd1c569efb8382187c43b89267b0b186
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-09-23 23:40:40 -0400

    Initial Commit

---
+++ Project-Euler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project-Euler
+//
+//  Created by Nicholas Tomlin on 9/23/15.
+//  Copyright © 2015 Nicholas Tomlin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
